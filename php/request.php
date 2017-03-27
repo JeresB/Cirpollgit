294eeef8c90ee9435ed0fc0bd706cd1b514a0d06
JeresB <boiselet.jeremy@gmail.com>
2017-03-27 08:40:35 +0200

    Ajout du fichier constants.php + panel bootstrap

--- php/request.php
+++ php/request.php
@@ -1,30 +1,42 @@
 <?php
+	// We check the request type.
+	$request = substr($_SERVER['PATH_INFO'], 1);
 
-$request = explode("/", $_SERVER['PATH_INFO']);
+	// We check if the request is a module.
+	if (is_dir('../'.$request))
+	{
+		// We extract the module name.
+		$moduleName = substr($request, strrpos($request, '/') + 1);
+		sendHtmlAndJsData('polls', $request, $moduleName);
+	}
+	else
+  {
+		header('HTTP/1.1 400 Bad request');
+		exit;
+	}
 
-switch ($request[1]) {
-  case 'polls':
-      sendHtmlAndJsData('polls', $request[1]);
-    exit;
-  default:
-      header('HTTP/1.1 400 Bad request');
-    break;
-}
+	//----------------------------------------------------------------------------
+	//--- sendHtmlAndJsData ------------------------------------------------------
+	//----------------------------------------------------------------------------
+	function sendHtmlAndJsData($divId, $modulePath, $moduleName)
+	{
+	  // We create the data (Html and Js).
+	  $data = array ('html' => $modulePath.'/'.$moduleName.'.html',
+			'divId' => $divId, 'js' => $modulePath.'/'.$moduleName.'.js');
+		sendJsonData($data);
+	}
 
-function sendHtmlAndJsData($divId, $modulename) {
-  $data = array(
-    'html' => 'tools/'.$modulename.'/'.$modulename.'.html',
-    'divId' => $divId,
-    'js' => 'tools/'.$modulename.'/'.$modulename.'.js'
-  );
-
-  sendJsonData($data);
-}
-
-function sendJsonData($data) {
-  header('Content-Type: text/plain;charset=utf-8');
-  header('Cache-control: no-store, no-cache, must-revalidate');
-  header('Pragma: no-cache');
-
-  echo json_encode($data);
-}
+	//----------------------------------------------------------------------------
+	//--- sendJsonData -----------------------------------------------------------
+	//----------------------------------------------------------------------------
+	function sendJsonData($data, $code = 200)
+	{
+	  // We send the data to the client.
+	  header('Content-Type: text/plain; charset=utf-8');
+	  header('Cache-control: no-store, no-cache, must-revalidate');
+	  header('Pragma: no-cache');
+		header('HTTP/1.1 200 OK');
+	  echo json_encode($data);
+		exit;
+	}
+?>
